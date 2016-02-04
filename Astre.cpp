ab4fe25cc41b3433cac36922f767ef5fa58572ce
Watashiku <adelbeque@hotmail.fr>
2016-02-04 16:31:01 +0100

    Update Astre.cpp

--- Astre.cpp
+++ Astre.cpp
@@ -3,9 +3,9 @@
 Astre::Astre()
 {
     mass = 333054.253182; // par rapport a la Terre
-    radius = 0.00465475825; // distance Terre Soleil
+    radius = 0; // distance Terre Soleil
     double* p = new double[2];
-    p[0]=55;
+    p[0]=0;
     p[1]=0;
     pos = p;
     theta0 = 0;
